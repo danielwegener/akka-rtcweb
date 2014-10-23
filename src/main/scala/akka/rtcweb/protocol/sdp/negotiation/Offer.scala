f82bbec889b042807ba725d9f8197f5057712297
Daniel Wegener <daniel.wegener@holisticon.de>
2014-10-23 19:57:44 +0200

    further

---
+++ src/main/scala/akka/rtcweb/protocol/sdp/negotiation/Offer.scala
@@ -0,0 +1,12 @@
+package akka.rtcweb.protocol.sdp.negotiation
+
+import akka.rtcweb.protocol.sdp.SessionDescription
+
+/**
+ * @author Daniel Wegener (Holisticon AG)
+ */
+case class Offer(sessionDescription: SessionDescription) {
+
+  //require(sessionDescription)
+
+}
---
+++ src/main/scala/akka/rtcweb/protocol/sdp/negotiation/package.scala
@@ -0,0 +1,7 @@
+package akka.rtcweb.protocol.sdp
+
+/** This package implements SDP unicast session negotiation defined in <em>RFC 3264 - An Offer/Answer Model Session Description Protocol</em>
+ */
+package object negotiation {
+
+}
