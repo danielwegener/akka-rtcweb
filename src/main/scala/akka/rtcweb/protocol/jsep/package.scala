13d7b6d52bd237ba96c06558704b1c961272b4c2
Daniel Wegener <daniel@wegener.me>
2015-06-24 23:56:21 +0200

    update rfc reference

--- src/main/scala/akka/rtcweb/protocol/jsep/package.scala
+++ src/main/scala/akka/rtcweb/protocol/jsep/package.scala
@@ -2,7 +2,7 @@ package akka.rtcweb.protocol
 
 /**
  * This is an akka adoption of the  Javascript Session Establishment Protocol
- * [draft-ietf-rtcweb-jsep-07](http://tools.ietf.org/html/draft-ietf-rtcweb-jsep-07)
+ * [draft-ietf-rtcweb-jsep-09](http://tools.ietf.org/html/draft-ietf-rtcweb-jsep-09)
  *
  * Let [[jsep.RTCPeerConnection]] be your entry point and friend.
  *
